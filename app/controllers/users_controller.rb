2f189a9e69b10a3e323f069f444293cdb6834cf9
Skillachie <skillachie@ubuntu.(none)>
2011-10-02 15:25:28 -0700

    Home Page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def profile
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfview::Application.config.secret_token = 'c475a0c27124bddf3094e805ada2c859463d47410ba799e60ea985d80c607087e94a41a0c3f3312ce2206ee4fdd464fdf1da3cb21028465c107b6714eff797dc'
