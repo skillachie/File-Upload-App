d24bec0660cab2a4698fce016aeb399df2ea882e
Skillachie <dwaynecampbell13@gmail.com>
2013-04-19 21:22:09 -0400

    updated project for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(:email => 'admin@upload.com',:password => 'admin',:password_confirmation => 'admin')
+user.toggle!(:admin)
+user.save
